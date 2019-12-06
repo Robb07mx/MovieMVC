64183faa6816afb6891cdb1e6f4f421ca50eb5b9
Rob917 <50025116+Rob917@users.noreply.github.com>
2019-12-06 16:18:49 -0600

    Add project files.

---
+++ MvcMovie/Controllers/DefaultHelloController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class DefaultHelloController : Controller
+    {
+        // GET: DefaultHello
+        public ActionResult Index() {
+            return View();
+        }
+
+        // GET: //DefuaultHello/Welcome
+        public ActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewBag.Message = "Hello" + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
+        }
+        
+    }
+}
